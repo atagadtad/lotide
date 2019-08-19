591fa61905f6996d9b8684da9975d0af6abb9725
atagadtad <aarontagadtad@gmail.com>
2019-08-19 18:23:59 +0000

    completed assertEqual assignment

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`😄 Assertion Passed: ${actual} === ${expected}`) : console.log(`😡 Assertion Failed: "${actual}" !== "${expected}"`);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
