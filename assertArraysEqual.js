f8eaa7f8b86c506a4486561ef9495952da05b03e
atagadtad <aarontagadtad@gmail.com>
2019-08-20 23:05:07 +0000

    finished assertArraysEqual assignment

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+
+
+const assertArraysEqual = function(array1, array2) {
+  for (i = 0; i < array1.length; i++) {
+    if (array1[i] !==  array2[i]) {
+      console.log(`😡 Assertion Failed: "${array1}" !== "${array2}"`);
+      return false;
+    } 
+  }
+  console.log(`😄 Assertion Passed: ${array1} === ${array2}`)
+  return true;
+};
+
+assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+
